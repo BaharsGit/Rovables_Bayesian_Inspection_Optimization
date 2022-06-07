2d0e94eb05065ed94283e10c26073a2d2062fbc0
SSR <zevminskyprimus@gmail.com>
2022-06-07 16:50:09 -0400

    fixed world file name in simSetup.py to match what's created by createWorld.py

--- pso_self-assembly_aws/python_code/simSetup.py
+++ pso_self-assembly_aws/python_code/simSetup.py
@@ -26,12 +26,12 @@ if not os.path.exists(tempDir):
     os.makedirs(tempDir)
 
 # #Create World file with randomized positions and walks based on robot seed
-# # world_seed not used in this case as the texture is fixed. 
+# # world_seed not used in this case as the texture is fixed.
 WG = WorldGenerator(world_seed=16, robot_seed=args.seed, robot_number=4)
 WG.createWorld()
 
 # #Run the Webots simulation
-dir = "../worlds/bayes_bot_sim" + "_" + str(args.seed) + ".wbt"
+dir = "../worlds/bayesian_rovables_sim" + "_" + str(args.seed) + ".wbt"
 subprocess.call(["webots --mode=fast --minimize --no-rendering --stdout --batch", dir], shell=True)
 # #time.sleep(30)
 
@@ -52,7 +52,7 @@ for file in os.listdir(tempDir):
     if file.endswith(".csv"):
         print("FILE FOUND")
         shutil.move(tempDir + file, folder + file)
-    else: 
+    else:
         print("NO FILE FOUND")
 
 os.rmdir(tempDir)
