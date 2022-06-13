59b7eebd3fcc56a3dcd0acf55a828078f57577a0
bhaghighat <baharsgit@gmail.com>
2022-06-13 17:43:42 -0400

    job file cleaned up

--- demo/jobfiles/job_lily_parallel.sh
+++ demo/jobfiles/job_lily_parallel.sh
@@ -1,161 +1,155 @@
 #!/bin/bash
-#$ -N lily
-#$ -S /bin/bash
-#$ -cwd
-#$ -j y
-##$ -m eas
-#$ -t 1-1
-##$ -p -512
 
-echo $(pwd)
+#
+# File: job_lily_parallel.sh shell script       
+# Date: 17 March 2017     
+# Description: This shell script was based on cluster job file job_lily_parallel.sub, 
+#              it runs on AWS (particle) nodes, it launches Webots to evaluate the corresponding particle
+# Author: Bahar Haghighat 
+# Modifications: by Bahar Haghighat, 20 May 2022, cleaned up non-AWS stuff left from local cluster launch
+#
+ 
+# The first input variable to the shell script is the GEN_ID, the second one is the PARTICLE_ID
 GEN_ID=$1 
-INDIVID_ID=$2
-# GEN_ID=0
-# INDIVID_ID=1
+PARTICLE_ID=$2
+
+
+# Setting the worst case fitness value, in case the launch doesn't go well, the worst case fitness value is considered as default
 WORST_FITNESS=43200
 
-WB_TIMEOUT=7200           # Kill webots after WB_TIMEOUT seconds
-#WB_TIMEOUT=15           # Kill webots after WB_TIMEOUT seconds
- 
-N_RUNS=1 #number of runs to be done
 
+# Kill webots after WB_TIMEOUT seconds
+WB_TIMEOUT=7200  
+
+ 
+# N_RUNS can be used for noise resistant evaluation of a particle
+# N_RUNS is number of evaluations of this particle by this shell script
+N_RUNS=1 
+
+
+# PATH : This line defines the path to the Webots world to be launched, should be modified for a different world name
+#WEBWORLD="../../worlds/24Lilies_LaLn.wbt"
 WEBWORLD="../../worlds/bayes_pso.wbt"
 
-# set the input directory (relative to the working directory)
+
+# Set the input directory (relative to the current working directory)
 INPUT_DIR=Generation_${GEN_ID}
-echo ${INPUT_DIR}
+echo "job_lily_parallel.sh input directory is " ${INPUT_DIR}
 
 
-# Output directory to put results
+# Set the output directory to put results (relative to the current working directory)
 OUTPUT_DIR=Generation_${GEN_ID}
-echo ${OUTPUT_DIR}
+echo "job_lily_parallel.sh output directory is " ${OUTPUT_DIR}
 
-# set the job base directory 
-JOB_BASE_DIR=$(pwd)/tmp/job${GEN_ID}_${INDIVID_ID}
-echo ${JOB_BASE_DIR}
 
-# create the working directory
+# Set the job base directory, the Webots working directory
+# pwd is acronym for print working directory (pwd), returns current directory
+JOB_BASE_DIR=$(pwd)/tmp/job${GEN_ID}_${PARTICLE_ID}
+ 
 
+# Create the working directory for Webots, where Webots can write its stuff
 if [ ! -d $JOB_BASE_DIR ]
 
 then
 
-   echo "(`date`) Create base directory $JOB_BASE_DIR"
-
+   echo "(`date`) Create job base directory for the Webots instance of this job_lily_parallel.sh script as $JOB_BASE_DIR"
    mkdir -p $JOB_BASE_DIR
 
 fi
 
- 
-# set the home directory locally so that Webots writes its log files there (dirty hack)
-
-#export HOME=$JOB_BASE_DIR
-
-# set the home directory locally so that Webots writes its stuff there
-#export HOME=/home/${USER}
+# Set Webots working directory, where Webots can write its stuff
 export WB_WORKING_DIR=$JOB_BASE_DIR
 
-echo "(`date`) Performing $N_RUNS runs for the following parameters:"
 
+echo "(`date`) Performing a total of $N_RUNS runs for particle $PARTICLE_ID"
  
-# perform the runs
-TRIAL_COUNTER=1
+# if Webots crashes after its launch, or fails to create local_fitness.txt, the script will do a number of follow-up trials
+TRIAL_COUNT=1
 
 for (( RUN_ID=1; RUN_ID<=$N_RUNS; RUN_ID++ )) 
 
-do
+   do
 
-   #export WB_WORKING_DIR=${JOB_BASE_DIR}
+      # copy the input parameters from the Generation_${GEN_ID} directory to Webots working directory 
+      cp ${INPUT_DIR}/prob_${PARTICLE_ID}.txt $WB_WORKING_DIR/prob.txt 
+      
+      echo "(`date`) Launching Webots (run $RUN_ID) of particle $PARTICLE_ID"
+      
+      # launch webots
+      # logs are redirected to webots_log.txt file
+      # timeout $WB_TIMEOUT xvfb-run webots --batch --mode=fast --stdout --stderr --no-rendering $WEBWORLD &> $WB_WORKING_DIR/webots_log.txt 
+      timeout $WB_TIMEOUT xvfb-run webots --batch --mode=fast --stdout --stderr --no-rendering $WEBWORLD
+    
 
- 
+      # waiting just a while before copying the output
+      sleep 1
 
-   # create the directory for this run
 
-   #mkdir $WB_WORKING_DIR
-
-   # copy the input parameters
-
-   cp ${INPUT_DIR}/prob_${INDIVID_ID}.txt $WB_WORKING_DIR/prob.txt
-   #echo $WB_WORKING_DIR/prob.txt
-   
-   
-
-   echo "(`date`) Launching webots (run $RUN_ID)"
-
-   
-   # launch webots
-   timeout $WB_TIMEOUT  xvfb-run webots --batch --mode=fast --stdout --stderr --no-rendering $WEBWORLD &> $WB_WORKING_DIR/webots_log.txt # logs are redirected to a txt file
- 
-
-   # waiting just a while before copying the output
-
-   sleep 1
-
- 
-
-   # checking whether run was successful
-
-   if [ -e $WB_WORKING_DIR/local_fitness.txt ] 
-
-   then
-
-      echo "(`date`) Run $RUN_ID successfully completed"
-
-      rm $WB_WORKING_DIR/ack.txt
-
-   else
-
-      echo "(`date`) Run $RUN_ID failed, cleaning up, and running again"
-
-      ((TRIAL_COUNTER--))
-
-      if [ $TRIAL_COUNTER -gt 0 ]
-
+      # checking whether run was successful
+      # the local_fitness.txt is written by the supervisor in Webots
+      if [ -e $WB_WORKING_DIR/local_fitness.txt ] 
       then
 
-      	# performing the run again
+         echo "(`date`) Run $RUN_ID successfully completed"
+         rm $WB_WORKING_DIR/ack.txt
 
-      	((RUN_ID--))
+      else
 
-      else 
+         echo "(`date`) Run $RUN_ID failed, cleaning up, and running again"
+         ((TRIAL_COUNT--))
+         # ToDo: perform cleanup of unwanted files
 
-        echo "(`date`) RUN got stuck, auxilary fitness file created"
+         if [ $TRIAL_COUNT -gt 0 ]
+         then
 
-	echo ${WORST_FITNESS} >> $WB_WORKING_DIR/local_fitness.txt
+         	# performing the run again
+         	((RUN_ID--))
+            # launch webots
+            # logs are redirected to webots_log.txt file
+            # timeout $WB_TIMEOUT xvfb-run webots --batch --mode=fast --stdout --stderr --no-rendering $WEBWORLD &> $WB_WORKING_DIR/webots_log.txt
 
-	rm $WB_WORKING_DIR$/ack.txt
+         else 
+
+           echo "(`date`) RUN got stuck, auxilary fitness file created"
+   	     echo ${WORST_FITNESS} >> $WB_WORKING_DIR/local_fitness.txt
+   	     rm $WB_WORKING_DIR$/ack.txt
+
+         fi
 
       fi
+    
+      sleep 1
 
-   fi
+   done
 
- 
 
-   sleep 1
+# Create a desired final output directory
+# OUTPUT_DIR=Generation_${GEN_ID}
+# if [ ! -d $OUTPUT_DIR ]
+# then
+# 	echo "(`date`) Create output directory $OUTPUT_DIR"
+# 	mkdir $OUTPUT_DIR
+# fi
 
-done
 
- 
 
- # create the output directory
-if [ ! -d $OUTPUT_DIR ]
-then
-	echo "(`date`) Create output directory $OUTPUT_DIR"
-	mkdir $OUTPUT_DIR
-fi
-# move results to output dir
+# Move results to the final output directory
 
-#mv $WB_WORKING_DIR/global_best_fitness.txt $OUTPUT_DIR/global_best_fitness_${INDIVID_ID}.txt
-#mv $WB_WORKING_DIR/global_best_prob.txt $OUTPUT_DIR/global_best_prob_${INDIVID_ID}.txt
-#mv $WB_WORKING_DIR/local_best_prob.txt $OUTPUT_DIR/local_best_prob_${INDIVID_ID}.txt
-mv $WB_WORKING_DIR/local_fitness.txt ${OUTPUT_DIR}/local_fitness_${INDIVID_ID}.txt
-#mv $WB_WORKING_DIR/prob.txt $OUTPUT_DIR/prob_${INDIVID_ID}.txt
-#mv $WB_WORKING_DIR/prob_w.txt $OUTPUT_DIR/prob_w_${INDIVID_ID}.txt
-#mv $WB_WORKING_DIR/rate_log.txt $OUTPUT_DIR/rate_log_${INDIVID_ID}.txt
-#mv $WB_WORKING_DIR/repeat_counter.txt $OUTPUT_DIR/repeat_counter_${INDIVID_ID}.txt
-#mv $WB_WORKING_DIR/run_counter.txt $OUTPUT_DIR/run_counter_${INDIVID_ID}.txt
-#mv $WB_WORKING_DIR/ttime.txt $OUTPUT_DIR/ttime_${INDIVID_ID}.txt
+#mv $WB_WORKING_DIR/global_best_fitness.txt $OUTPUT_DIR/global_best_fitness_${PARTICLE_ID}.txt
+#mv $WB_WORKING_DIR/global_best_prob.txt $OUTPUT_DIR/global_best_prob_${PARTICLE_ID}.txt
+#mv $WB_WORKING_DIR/local_best_prob.txt $OUTPUT_DIR/local_best_prob_${PARTICLE_ID}.txt
 
-# remove job base directory
+mv $WB_WORKING_DIR/local_fitness.txt ${OUTPUT_DIR}/local_fitness_${PARTICLE_ID}.txt
+
+#mv $WB_WORKING_DIR/prob.txt $OUTPUT_DIR/prob_${PARTICLE_ID}.txt
+#mv $WB_WORKING_DIR/prob_w.txt $OUTPUT_DIR/prob_w_${PARTICLE_ID}.txt
+#mv $WB_WORKING_DIR/rate_log.txt $OUTPUT_DIR/rate_log_${PARTICLE_ID}.txt
+#mv $WB_WORKING_DIR/repeat_counter.txt $OUTPUT_DIR/repeat_counter_${PARTICLE_ID}.txt
+#mv $WB_WORKING_DIR/run_counter.txt $OUTPUT_DIR/run_counter_${PARTICLE_ID}.txt
+#mv $WB_WORKING_DIR/ttime.txt $OUTPUT_DIR/ttime_${PARTICLE_ID}.txt
+
+
+
+# Remove job base directory, the Webots working directory
 rm -r $JOB_BASE_DIR
-echo "(`date`) Job ${GEN_ID}_${INDIVID_ID} completed"
+echo "(`date`) Job ${GEN_ID}_${PARTICLE_ID} completed by particle $PARTICLE_ID on node $($(PARTICLE_ID)+1)"
