be15b8dc108fa1c623b0e93d07f1f3422b84a8d5
darren-chiu <darrenchiu321@ghmail.com>
2023-01-30 01:52:04 -0500

    updated bounds

--- demo/jobfiles/PSO_tocluster.py
+++ demo/jobfiles/PSO_tocluster.py
@@ -383,21 +383,25 @@ time_to_cross_arena = 36125.079475174839899 #ms
 step_to_cross_arena = time_to_cross_arena / time_step #time steps 
 # F(R_FWD, TAO, Velicty) - Max samples taken when crossing arena
 # CHECK INITIAL VALUES
-if (PARTICLE_SET == 1): # SET ONE 
-    bounds = [0, 500, tao_square/3, tao_square*3, tao_square/3, step_to_cross_arena, 5, 95, 0, step_to_cross_arena, 0, 0]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
-    x0=[10, 200, 200, 50, step_to_cross_arena/2, 0] #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Observation Wait Time]
+multiplier = 5
+if (PARTICLE_SET == 1): 
+    bounds = [0, 500, tao_square/multiplier, tao_square*multiplier, tao_square/multiplier, step_to_cross_arena, 5, 95, tao_square/multiplier, step_to_cross_arena, 0, 0]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
+    x0=[50, tao_square*2, tao_square, 50, step_to_cross_arena/multiplier, 0] #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Observation Wait Time]
 if (PARTICLE_SET == 2):
-    bounds = [10, 500, 10, 350, 20, 3000, 10, 90, 0, 0, 10, 250]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
-    x0=[10, 200, 200, 30, 0, 15] #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Obs Wait Time]
+    bounds = [0, 500, tao_square/multiplier, tao_square*multiplier, tao_square/multiplier, step_to_cross_arena, 5, 95, 0, 0, 0, 0]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
+    x0=[50, tao_square*2, tao_square, 50, 0, 0] #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Obs Wait Time]
 if (PARTICLE_SET == 3):
-    bounds = [10, 10, tao_square/3, tao_square*3, tao_square/3, step_to_cross_arena, 5, 95, 0, step_to_cross_arena, 0, 0]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
-    x0=[10, 200, 200, 50, step_to_cross_arena/2, 0]  #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Obs Wait Time]
+    bounds = [10, 10, tao_square/multiplier, tao_square*multiplier, tao_square/multiplier, step_to_cross_arena, 5, 95, tao_square/multiplier, step_to_cross_arena, 0, 0]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
+    x0=[10, tao_square*2, tao_square, 50, step_to_cross_arena/multiplier, 0]  #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Obs Wait Time]
 if (PARTICLE_SET == 4):
-    bounds = [0, 0, 10, 350, 20, 3000, 10, 90, 0, 0, 10, 250]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
-    x0=[10, 200, 200, 30, 0, 15]  #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Obs Wait Time]
+    bounds = [10, 10, tao_square/multiplier, tao_square*multiplier, tao_square/multiplier, step_to_cross_arena, 5, 95, 0, 0, 0, 0]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
+    x0=[10, tao_square*2, tao_square, 50, 0, 0]  #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Obs Wait Time]
 if (PARTICLE_SET == 5): # SET ONE 
-    bounds = [0, 0, tao_square/3, tao_square*3, tao_square/3, step_to_cross_arena, 5, 95, tao_square/3, step_to_cross_arena, 0, 0]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
-    x0=[0, tao_square*2, tao_square, 50, step_to_cross_arena/5, 0] #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Observation Wait Time]
+    bounds = [0, 0, tao_square/multiplier, tao_square*multiplier, tao_square/multiplier, step_to_cross_arena, 5, 95, tao_square/multiplier, step_to_cross_arena, 0, 0]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
+    x0=[0, tao_square*2, tao_square, 50, step_to_cross_arena/multiplier, 0] #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Observation Wait Time]
+if (PARTICLE_SET == 6): # SET ONE 
+    bounds = [0, 0, tao_square/multiplier, tao_square*multiplier, tao_square/multiplier, step_to_cross_arena, 5, 95, 0, 0, 0, 0]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
+    x0=[0, tao_square*2, tao_square, 50, 0, 0] #[Alpha, Tao, Random Forward, CA Trigger, Hysterisis, Observation Wait Time]
 if (PARTICLE_SET == 7):
     # THIS IS USED WITH THE TEST OPTIMIZATION FUNCTION
     bounds = [0, 10000, 0, 10000, 0, 10000, 0, 10000, 0, 10000, 0, 10000]  # input bounds [(x1_min,x1_max, x2_min, x2_max, . . .)]
